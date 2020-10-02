38743b488817ddc75e92ba188f97c4b57855faab
Cedric Tripps <ceddy-98@gmx.de>
2020-10-02 10:10:23 +0200

    Skript vereinfacht

--- SHA256_erstellen.py
+++ SHA256_erstellen.py
@@ -1,38 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[8]:
-
-
-eingabe = input("Text: ")
-
-
-# In[2]:
-
-
 from hashlib import sha256
 
-
-# In[9]:
-
-
+eingabe = input("Text: ")
 hashCode = sha256(eingabe.encode('utf-8'))
-
-
-# In[10]:
-
-
-hashCode
-
-
-# In[11]:
-
-
-hashCode.hexdigest()
-
-
-# In[ ]:
-
-
-
-
+print("Hashcode: " + hashCode.hexdigest())
